4a247c119241bdb374399242a7b26bcca0b4d2da
GODxWOLF <malhotrakrish2106@gmail.com>
2025-03-16 02:06:17 +0530

    page.tsx

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import Calculator from "@/components/calculator"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-br from-slate-900 to-slate-800">
+      <Calculator />
+    </main>
+  )
+}
+
